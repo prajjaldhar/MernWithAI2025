c2fd52114de7881b66971b17efd9c10bff3b233b
prajjal <prajjaldhar41@gmail.com>
2025-06-09 21:08:30 +0530

    user dashboard added

--- Academica/src/App.jsx
+++ Academica/src/App.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import DashboardLayout from "./Layout/DashboardLayout";
+import UserDetails from "./Pages/Dashboard/UserDetails";
+import CourseDetails from "./Pages/Dashboard/CourseDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* You can add more routes here later, like /courses, /about, etc. */}
+        {/* Dashboard Routes */}
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Navigate to="userdetails" replace />} />
+          <Route path="userdetails" element={<UserDetails />} />
+          <Route path="coursedetails" element={<CourseDetails />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
